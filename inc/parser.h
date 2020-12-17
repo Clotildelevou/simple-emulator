be2737475a07682717c0e3748c81dcf38fb87e75
Maxime Mehio <maxime.mehio@epita.fr>
2020-12-18 00:37:33 +0100

    parsing branches and ldr str

--- inc/parser.h
+++ inc/parser.h
@@ -3,56 +3,104 @@
 
 #include <stdint.h>
 
-
 enum cond {
-    EQZ = 0b0000,
-    NEZ = 0b0001,
-    NE  = 0b0010,
-    MIN = 0b0100,
-    PLN = 0b0101,
-    VSV = 0b0110,
-    VCV = 0b0111,
-    HIC = 0b1000,
-    LSC = 0b1001,
-    GEN = 0b1010,
-    LTN = 0b1011,
-    GTZ = 0b1100,
-    LEZ = 0b1101,
-    AL  = 0b1110,
-    ERR = 0b1111,
-};
-enum data_operation {
-    AND = 0b0000,
-    EOR = 0b0001,
-    SUB = 0b0010,
-    RSB = 0b0011,
-    ADD = 0b0100,
-    ADC = 0b0101,
-    SBC = 0b0110,
-    RSC = 0b0111,
-    TST = 0b1000,
-    TEQ = 0b1001,
-    CMP = 0b1010,
-    CMN = 0b1011,
-    ORR = 0b1100,
-    MOV = 0b1101,
-    BIC = 0b1110,
-    MVN = 0b1111
+    COND_EQZ = 0b0000,
+    COND_NEZ = 0b0001,
+    COND_NE  = 0b0010,
+    COND_CC  = 0b0011,
+    COND_MIN = 0b0100,
+    COND_PLN = 0b0101,
+    COND_VSV = 0b0110,
+    COND_VCV = 0b0111,
+    COND_HIC = 0b1000,
+    COND_LSC = 0b1001,
+    COND_GEN = 0b1010,
+    COND_LTN = 0b1011,
+    COND_GTZ = 0b1100,
+    COND_LEZ = 0b1101,
+    COND_AL  = 0b1110,
+    COND_ERR = 0b1111,
 };
 
+enum data_processing {
+    DP_AND = 0b0000,
+    DP_EOR = 0b0001,
+    DP_SUB = 0b0010,
+    DP_RSB = 0b0011,
+    DP_ADD = 0b0100,
+    DP_ADC = 0b0101,
+    DP_SBC = 0b0110,
+    DP_RSC = 0b0111,
+    DP_TST = 0b1000,
+    DP_TEQ = 0b1001,
+    DP_CMP = 0b1010,
+    DP_CMN = 0b1011,
+    DP_ORR = 0b1100,
+    DP_MOV = 0b1101,
+    DP_BIC = 0b1110,
+    DP_MVN = 0b1111
+};
+
+extern const char *const data_pro_str[];
+extern const char *const cond_str[];
+
+struct reg {
+    uint8_t rm;
+    uint8_t shift;
+};
+
+struct lmm {
+    uint8_t rotate;
+    uint8_t lmm;
+};
+
+union dt_operand2 {
+    struct lmm lmm;
+    struct reg reg;
+};
+
+struct data_processing_inst {
+    enum data_processing opcode;
+    uint8_t             s;
+    uint8_t             rn;
+    uint8_t             rd;
+    uint8_t             i;
+    union dt_operand2   op2;
+};
+
+struct branch_inst {
+    uint8_t  link;
+    uint32_t address;
+};
+
+enum instruction_type {
+    INST_T_BRANCH,
+    INST_T_DATA_PROCESSING,
+    INST_T_SINGLE_DATA_TRANSFER,
+    INST_T_NOT_IMPLEMENTED_YET
+};
+
+struct single_data_transfer {
+    uint8_t ipubwl; //bitmask
+    uint8_t rn;
+    uint8_t rd;
+    uint32_t offset;
+};
+
+union instructions {
+    struct data_processing_inst data_proc;
+    struct branch_inst          branch;
+    struct single_data_transfer sd_transfer;
+};
 
 struct instruction {
-    int succesfully_parsed;
-    enum data_operation op;
-    enum cond cond;
-    char s;
-    char rn;
-    char rd;
-    char i;
+    enum cond             cond;
+    enum instruction_type type;
+    union instructions    inst;
 };
 
-struct instruction parse_single(const uint32_t inst);
-void print_parsed_inst(struct instruction s);
-struct instruction* parse(const uint32_t* inst, unsigned nb_instructions);
+struct instruction  parse_single(const uint32_t inst);
+void                print_parsed_inst(struct instruction s);
+struct instruction *parse(const uint32_t *inst, unsigned nb_instructions);
 
 #endif /* PARSER_H */
--- src/parser.c
+++ src/parser.c
@@ -3,48 +3,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char *const data_op_string[] = {
-    [AND] = "AND", [EOR] = "EOR", [SUB] = "SUB", [RSB] = "RSB",
-    [ADD] = "ADD", [ADC] = "ADC", [SBC] = "SBC", [RSC] = "RSC",
-    [TST] = "TST", [TEQ] = "TEQ", [CMP] = "CMP", [CMN] = "CMN",
-    [ORR] = "ORR", [MOV] = "MOV", [BIC] = "BIC", [MVN] = "MVN"};
+const char *const data_op_str[] = {
+    [DP_AND] = "AND", [DP_EOR] = "EOR", [DP_SUB] = "SUB", [DP_RSB] = "RSB",
+    [DP_ADD] = "ADD", [DP_ADC] = "ADC", [DP_SBC] = "SBC", [DP_RSC] = "RSC",
+    [DP_TST] = "TST", [DP_TEQ] = "TEQ", [DP_CMP] = "CMP", [DP_CMN] = "CMN",
+    [DP_ORR] = "ORR", [DP_MOV] = "MOV", [DP_BIC] = "BIC", [DP_MVN] = "MVN"};
+const char *const branch_str[]  = {[0] = "b", [1] = "bl"};
+const char *const data_tr_str[] = {[0] = "str", [1] = "ldr"};
+
 const char *const cond_str[] = {
-    [EQZ] = "EQZ", [NEZ] = "NEZ", [NE] = "NE",   [MIN] = "MIN", [PLN] = "PLN",
-    [VSV] = "VSV", [VCV] = "VCV", [HIC] = "HIC", [LSC] = "LSC", [GEN] = "GEN",
-    [LTN] = "LTN", [GTZ] = "GTZ", [LEZ] = "LEZ", [AL] = "AL",   [ERR] = "ERR",
+    [COND_EQZ] = "EQZ", [COND_NEZ] = "NEZ",
+    [COND_NE] = "NE",   [COND_CC] = "CC",
+    [COND_MIN] = "MIN", [COND_PLN] = "PLN",
+    [COND_VSV] = "VSV", [COND_VCV] = "VCV",
+    [COND_HIC] = "HIC", [COND_LSC] = "LSC",
+    [COND_GEN] = "GEN", [COND_LTN] = "LTN",
+    [COND_GTZ] = "GTZ", [COND_LEZ] = "LEZ",
+    [COND_AL] = "AL",   [COND_ERR] = "ERROR ILL FORMED",
 };
-const unsigned char data_operation_filter   = 0b00001100;
-const unsigned char data_operation_rbitmask = 0b00000000;
-struct instruction  parse_single(const uint32_t inst) {
+
+const uint8_t data_processing_filter  = 0b00001100;
+const uint8_t data_processing_bitmask = 0b00000000;
+const uint8_t branch_filter           = 0b00001110;
+const uint8_t branch_bitmask          = 0b00001010;
+const uint8_t single_data_filter      = 0b00001100;
+const uint8_t single_data_bitmask     = 0b00000100;
+
+int test_inst_type(uint8_t byte, uint8_t filter, uint8_t bitmask) {
+    return !((byte & filter) ^ bitmask);
+}
+
+struct instruction parse_single(const uint32_t inst) {
+
     // parse
     uint32_t rinst = reverse_endianess(inst);
-    char *             data = (char *) &rinst;
+    char *   data  = (char *) &rinst;
+
     struct instruction res;
-    res.succesfully_parsed = 0;
+
     res.cond = (enum cond)(data[0] & 0b11110000) >> 4;
-    if (!((data[0] & data_operation_filter) ^ data_operation_rbitmask)) {
-        char op = 0;
-        op += (data[0] & 0x01) << 3;
+    if (test_inst_type(data[0], data_processing_filter,
+                       data_processing_bitmask)) {
+
+        res.type = INST_T_DATA_PROCESSING;
+        char op  = 0;
+
+        op += (data[0] & 1) << 3;
         op += (data[1] & 0b11100000) >> 5;
-        res.op                 = (enum data_operation) op;
-        res.i                  = data[0] & 0b00000010;
-        res.s                  = data[1] & 0b00010000;
-        res.rn                 = data[1] & 0b00001111;
-        res.rd                 = (data[2] & 0b11110000) >> 4;
-        res.succesfully_parsed = 1;
+
+        res.inst.data_proc.opcode = (enum data_processing) op;
+        res.inst.data_proc.i      = data[0] & 0b00000010;
+        res.inst.data_proc.s      = data[1] & 0b00010000;
+        res.inst.data_proc.rn     = data[1] & 0b00001111;
+        res.inst.data_proc.rd     = (data[2] & 0b11110000) >> 4;
+
+        if (res.inst.data_proc.i) {
+
+            // op2 not a register
+            res.inst.data_proc.op2.lmm.rotate = data[3] & 0b00001111;
+            res.inst.data_proc.op2.lmm.lmm    = data[4];
+
+        } else {
+
+            // op2 is a register
+            res.inst.data_proc.op2.reg.shift = (data[3] & 0b00001111) << 4;
+            res.inst.data_proc.op2.reg.shift = (data[4] & 0b11110000) >> 4;
+            res.inst.data_proc.op2.reg.rm    = data[4] & 0b00001111;
+        }
+
+    } else if (test_inst_type(data[0], branch_filter, branch_bitmask)) {
+
+        res.type                = INST_T_BRANCH;
+        res.inst.branch.link    = data[0] & 1;
+        res.inst.branch.address = (rinst & 0x00FFFFFF) << 2;
+
+    } else if (test_inst_type(data[0], single_data_filter,
+                              single_data_bitmask)) {
+        res.type = INST_T_SINGLE_DATA_TRANSFER;
+
+        res.inst.sd_transfer.ipubwl = (data[0] & 0b00000011) << 4;
+        res.inst.sd_transfer.ipubwl += (data[1] & 0b11110000) >> 4;
+        res.inst.sd_transfer.rn     = data[1] & 0b00001111;
+        res.inst.sd_transfer.rd     = (data[2] & 0b11110000) >> 4;
+        res.inst.sd_transfer.offset = (data[3] & 0b00001111) << 8;
+        res.inst.sd_transfer.offset += data[4];
+    } else {
+        res.type = INST_T_NOT_IMPLEMENTED_YET;
     }
 
     return res;
 }
 
 void print_parsed_inst(struct instruction s) {
-    if (!s.succesfully_parsed) {
+    if (s.type == INST_T_NOT_IMPLEMENTED_YET) {
         printf("not implemented yet, cond %s \n", cond_str[s.cond]);
-        return;
+    } else if (s.type == INST_T_BRANCH) {
+        printf("BR: %s %u \n", branch_str[s.inst.branch.link],
+               s.inst.branch.address);
+    } else if (s.type == INST_T_DATA_PROCESSING) {
+        printf("DP: %s rd %02x rn %02x i %hhx s %hhx ",
+               data_op_str[s.inst.data_proc.opcode], s.inst.data_proc.rd,
+               s.inst.data_proc.rn, s.inst.data_proc.i, s.inst.data_proc.s);
+        if (s.inst.data_proc.i) {
+            printf("op2 rot %02x lmm %02x\n", s.inst.data_proc.op2.lmm.rotate,
+                   s.inst.data_proc.op2.lmm.lmm);
+        } else {
+            printf("op2 shift %02x lmm %02x\n", s.inst.data_proc.op2.reg.shift,
+                   s.inst.data_proc.op2.reg.rm);
+        }
+    } else if (s.type == INST_T_SINGLE_DATA_TRANSFER) {
+        printf("DT: %s write_base %s byte/bit %s updown %s pre/post %s immediate %s\n",
+               data_tr_str[s.inst.sd_transfer.ipubwl & 1],
+               s.inst.sd_transfer.ipubwl & 0b10 ? "no write b": "write b",
+               s.inst.sd_transfer.ipubwl & 0b100 ? "byte" : "word",
+               s.inst.sd_transfer.ipubwl & 0b1000 ? "Up" : "Down",
+               s.inst.sd_transfer.ipubwl & 0b10000 ? "pre" : "post",
+               s.inst.sd_transfer.ipubwl & 0b100000 ? "reg" : "immediate");
+    } else {
+        printf("WHAT WHAT WHAT ?\n");
     }
-
-    printf("%s rd %02x rn %02x i %hhx s %hhx", data_op_string[s.op], s.rd, s.rn,
-           s.i, s.s);
 }
 
 struct instruction *parse(const uint32_t *inst, unsigned nb_instructions) {
